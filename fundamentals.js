455a0f647a1cf5469e2a936767e4e52fe1a00934
anace-m <maldonadoanaceciliac@johndeere.com>
2021-01-08 08:30:17 -0600

    fundamentals 4

--- fundamentals.js
+++ fundamentals.js
@@ -21,7 +21,10 @@ console.log(favoriteFoods);
 // #4: Create a `for` loop that adds each string in `foods` to `favoriteFoods`.
 // Type your solution immediately below this line:
 
-
+for (i=0; i<foods.length; i++) {
+    favoriteFoods.push(foods[i]);
+    console.log(favoriteFoods)
+;}
 
 // #5: Create an object literal called `instructor` that contains three key-value pairs.
 // Type your solution immediately below this line:
