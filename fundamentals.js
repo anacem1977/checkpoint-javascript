982e79da74fd31c110a7beb7bba78f524c38fc23
anace-m <maldonadoanaceciliac@johndeere.com>
2021-01-08 08:26:23 -0600

    fundamentals 1 & 2

--- fundamentals.js
+++ fundamentals.js
@@ -3,11 +3,14 @@
 // #1: Create an array of strings called `foods` that contains three foods.
 // Type your solution immediately below this line:
 
+var foods = ["French Fries", "Tacos", "Hamburguer"];
+console.log(foods);
 
 // #2: Access the last item in the array and assign to a variable called `last`.
 // Type your solution immediately below this line:
 
-
+var last = foods[2];
+console.log(last);
 
 // #3: Create an empty array called `favoriteFoods`.
 // Type your solution immediately below this line:
