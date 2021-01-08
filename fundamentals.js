334b1bcf39c6cfe1ff3ae76cbfadaf99e13b1791
anace-m <maldonadoanaceciliac@johndeere.com>
2021-01-08 08:46:53 -0600

    fundamentals 6 (not quite right

--- fundamentals.js
+++ fundamentals.js
@@ -36,3 +36,6 @@ console.log(instructor);
 // it (do not change the original object you typed above) and assigning it
 // a boolean value.
 // Type your solution immediately below this line:
+
+instructor.hasofficehours = "true";
+console.log(instructor);
